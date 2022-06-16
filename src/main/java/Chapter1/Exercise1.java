94b6497409f9b12e56577b051cac8280bdd0aefd
ats3md <ats3md@umsystem.edu>
2022-06-16 15:40:56 -0500

    initial commit

---
+++ src/main/java/Chapter1/Exercise1.java
@@ -0,0 +1,9 @@
+package Chapter1;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to computer Science");
+        System.out.println("Programming is fun");
+    }
+}
