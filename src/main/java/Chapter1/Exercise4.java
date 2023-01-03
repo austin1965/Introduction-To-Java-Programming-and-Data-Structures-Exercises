702a9321538602f2688a541f800ba7b36a97a00b
austin1965 <83925251+austin1965@users.noreply.github.com>
2023-01-02 18:42:21 -0600

    performed exercise 4

---
+++ src/main/java/Chapter1/Exercise4.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+public class Exercise4 {
+
+    public static void main(String[] args) {
+        System.out.println("a  a^2  a^3");
+        for (int i = 1; i < 5; i++) {
+            System.out.println(i + "   " + i*i + "   " + i*i*i);
+        }
+    }
+
+}
